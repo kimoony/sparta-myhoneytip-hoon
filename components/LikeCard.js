0f8bc737d01a06630be0e26f0c5da6456949d670
kimoony <hoonby9011@gmail.com>
2021-11-05 02:57:50 +0900

    update

--- components/LikeCard.js
+++ components/LikeCard.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Image, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import { firebase_db } from "../firebaseConfig"
+import Constants from 'expo-constants';
+
 
 //MainPage로 부터 navigation 속성을 전달받아 Card 컴포넌트 안에서 사용
 export default function LikeCard({ content, navigation }) {
+
+  const detail = () => {
+    navigation.navigate('DetailPage', { idx: content.idx })
+  }
+
+  const remove = () => {
+    const user_id = Constants.installationId;
+    firebase_db.ref('/like/' + user_id + '/' + content.idx).remove().then(function () {
+      Alert.alert("삭제 완료");
+      navigation.navigate('LikePage')
+    })
+  }
   return (
     //카드 자체가 버튼역할로써 누르게되면 상세페이지로 넘어가게끔 TouchableOpacity를 사용
     <View style={styles.card}>
@@ -11,6 +26,12 @@ export default function LikeCard({ content, navigation }) {
         <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
         <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
         <Text style={styles.cardDate}>{content.date}</Text>
+
+        <View style={styles.buttonGroup}>
+          <TouchableOpacity style={styles.button} onPress={() => detail()}><Text style={styles.buttonText}>자세히보기</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => remove()}><Text style={styles.buttonText}>찜 해제</Text></TouchableOpacity>
+
+        </View>
       </View>
     </View>
   )
@@ -48,5 +69,22 @@ const styles = StyleSheet.create({
   cardDate: {
     fontSize: 10,
     color: "#A6A6A6",
+  },
+  buttonGroup: {
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginTop: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'deeppink',
+    borderRadius: 7
+  },
+  buttonText: {
+    color: 'deeppink',
+    textAlign: 'center'
   }
 });
